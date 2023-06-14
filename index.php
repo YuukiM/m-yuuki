4a5c9cfc58ce7f637b3e13cae35aee0d9a399233
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2023-06-14 14:08:38 +0900

    マージンの取り方を変更

--- index.php
+++ index.php
@@ -100,59 +100,61 @@
 				<h2 class="section__heading">
 					経歴
 				</h2>
-				<ul class="biography-list">
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2022</dt>
-							<dd>講師歴2年。400名以上の受講生に教えてきました。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2020</dt>
-							<dd>Webデザイン講師としてデビュー。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2017 – 2020</dt>
-							<dd>フリーランスWebデザイナーとして活動開始。アパレル系ECサイトのコーディングや大手企業のLP、製品ページのコーディングなど。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2013 - 2016</dt>
-							<dd>
-								外資系企業にて、大手ソフトウェアメーカーの公式ECサイトやLPなどのフロント開発を行う。個人では中小企業向けのサイトをWordpressで制作。デザインからテーマの開発、実装まで手掛ける。
-							</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2010 - 2013</dt>
-							<dd>都内ウェブ制作会社にて、Wordpressを使用したウェブサイトの制作を受け持つ。デザインからテーマの開発、実装までを担当。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2008</dt>
-							<dd>Webクリエイターとして活動を始める。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>2001</dt>
-							<dd>フランスに語学留学。4年半滞在。</dd>
-						</dl>
-					</li>
-					<li class="biography-list__item">
-						<dl class="biography-list__text">
-							<dt>1998</dt>
-							<dd>高校2年生。はじめてHTMLでウェブサイトを作る。</dd>
-						</dl>
-					</li>
-				</ul>
-				<a href="https://m-yuuki.com/profile/" target="_blank" class="button">詳しい職務経歴</a>
+				<div class="section__wrap">
+					<ul class="biography-list">
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2022</dt>
+								<dd>講師歴2年。400名以上の受講生に教えてきました。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2020</dt>
+								<dd>Webデザイン講師としてデビュー。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2017 – 2020</dt>
+								<dd>フリーランスWebデザイナーとして活動開始。アパレル系ECサイトのコーディングや大手企業のLP、製品ページのコーディングなど。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2013 - 2016</dt>
+								<dd>
+									外資系企業にて、大手ソフトウェアメーカーの公式ECサイトやLPなどのフロント開発を行う。個人では中小企業向けのサイトをWordpressで制作。デザインからテーマの開発、実装まで手掛ける。
+								</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2010 - 2013</dt>
+								<dd>都内ウェブ制作会社にて、Wordpressを使用したウェブサイトの制作を受け持つ。デザインからテーマの開発、実装までを担当。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2008</dt>
+								<dd>Webクリエイターとして活動を始める。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>2001</dt>
+								<dd>フランスに語学留学。4年半滞在。</dd>
+							</dl>
+						</li>
+						<li class="biography-list__item">
+							<dl class="biography-list__text">
+								<dt>1998</dt>
+								<dd>高校2年生。はじめてHTMLでウェブサイトを作る。</dd>
+							</dl>
+						</li>
+					</ul>
+					<a href="https://m-yuuki.com/profile/" target="_blank" class="button">詳しい職務経歴</a>
+				</div>
 			</div>
 			<div class="image-area biography__image">
 				<div class="letters">
@@ -171,76 +173,77 @@
 				<h2 class="section__heading">
 					スキル
 				</h2>
-				<ul class="skills__primary">
-					<li class="skill-item">
-						<div class="skill-item__text">
-							<h3 class="skill-item__heading item-heading">
-								Webデザイン講師
-							</h3>
-							<div class="skill-item__detail">
-								<p>
-									オンライン、対面レッスンに対応しています。
-								</p>
-								<p>
-									女性向けWebデザインスクールで「非常に良い」との評価を多数いただいています。
-								</p>
-								<p>
-									大手オンラインスクールの評価でも、総合評価で5点中4.6点をいただきました。
+				<div class="section__wrap">
+					<ul class="skills__primary">
+						<li class="skill-item">
+							<div class="skill-item__text">
+								<h3 class="skill-item__heading item-heading">
+									Webデザイン講師
+								</h3>
+								<div class="skill-item__detail">
+									<p>
+										オンライン、対面レッスンに対応しています。
+									</p>
+									<p>
+										女性向けWebデザインスクールで「非常に良い」との評価を多数いただいています。
+									</p>
+									<p>
+										大手オンラインスクールの評価でも、総合評価で5点中4.6点をいただきました。
+									</p>
+								</div>
+							</div>
+							<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+									 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
+									d="M7.5 21.5v-1h4v-3H3c-.3 0-.5-.2-.5-.5V4c0-.3.2-.5.5-.5h18c.3 0 .5.2.5.5v13c0 .3-.2.5-.5.5h-8.5v3h4v1h-9zm-4-5h17v-12h-17v12z"/>
+								<path d="M14.1 11.2c0 2.7-4.3 2.7-4.2 0-.1-2.7 4.3-2.7 4.2 0zm1.1 5.3c.1-4.1-6.4-4.1-6.4 0h6.4z"
+											style="stroke-width:.5;stroke-miterlimit:10"/></svg>
+						</li>
+					</ul>
+					<ul class="skills__secondary">
+						<li class="skill-item">
+							<div class="skill-item__text">
+								<h3 class="skill-item__heading item-heading">
+									コーディング
+								</h3>
+								<div class="skill-item__detail">
+									<p>
+										HTML Living Standard、CSS、jQueryを用いて、メンテナンス性を考慮したサイト構築を目指します。BEMや、Sassも使用できます。使用ツールはPHPStorm、VSCodeなど
+									</p>
+								</div>
+							</div>
+							<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+									 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
+									d="M9.3 18.4 14 5.6h.8l-4.7 12.8h-.8zM.7 12l4.9-4.9.7.7L2.1 12l4.2 4.2-.6.8-5-5zm16.9 4.2 4.2-4.2-4.2-4.2.7-.7 5 4.9-5 5-.7-.8z"/></svg>
+						</li>
+						<li class="skill-item">
+							<div class="skill-item__text">
+								<h3 class="skill-item__heading item-heading">
+									Wordpress
+								</h3>
+								<p class="skill-item__detail">
+									Wordpress歴10年以上。バージョン2.3の時代からWPでのサイト構築、オリジナルテーマを使用したサイト作成をしています。テンプレートの様々な導入パターンにも柔軟に対応可能です。
 								</p>
 							</div>
-						</div>
-						<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
-								d="M7.5 21.5v-1h4v-3H3c-.3 0-.5-.2-.5-.5V4c0-.3.2-.5.5-.5h18c.3 0 .5.2.5.5v13c0 .3-.2.5-.5.5h-8.5v3h4v1h-9zm-4-5h17v-12h-17v12z"/>
-							<path d="M14.1 11.2c0 2.7-4.3 2.7-4.2 0-.1-2.7 4.3-2.7 4.2 0zm1.1 5.3c.1-4.1-6.4-4.1-6.4 0h6.4z"
-										style="stroke-width:.5;stroke-miterlimit:10"/></svg>
-					</li>
-				</ul>
-				<ul class="skills__secondary">
-					<li class="skill-item">
-						<div class="skill-item__text">
-							<h3 class="skill-item__heading item-heading">
-								コーディング
-							</h3>
-							<div class="skill-item__detail">
-								<p>
-									HTML Living Standard、CSS、jQueryを用いて、メンテナンス性を考慮したサイト構築を目指します。BEMや、Sassも使用できます。使用ツールはPHPStorm、VSCodeなど
+							<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+									 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
+									d="M12 21.8c-2.6 0-5.1-1-6.9-2.9C3.2 17 2.2 14.6 2.2 12c0-2.6 1-5.1 2.9-6.9S9.4 2.2 12 2.2c2.6 0 5.1 1 6.9 2.9 1.9 1.9 2.9 4.3 2.9 6.9 0 2.6-1 5.1-2.9 6.9-1.8 1.9-4.3 2.9-6.9 2.9zm-2.7-1.3.2.1c.8.2 1.6.4 2.5.4 1 0 2-.2 3-.5l.2-.1-.2-.4-2.9-7.8-2.8 8.3zM3.9 8.4c-.5 1.1-.8 2.3-.8 3.6 0 3.4 1.9 6.5 5 8l.4.2L4.1 7.9l-.2.5zm15.9-.7c0 1-.8 3.4-1 4L16 19.9l.5-.3c1.3-.8 2.5-1.9 3.2-3.2.8-1.3 1.2-2.9 1.2-4.4 0-1.5-.4-3-1.1-4.3zM7.6 7.3c.1 0 .2.1.2.2 0 0 0 .1-.1.2 0 0-.5.1-1 .1h-.3L9.8 18l2.1-6.3-1.4-3.9h-.1c-.3 0-.6 0-.9-.1-.1 0-.2-.1-.1-.2 0 0 0-.2.2-.2.1 0 1.5.1 2.3.1.9 0 2.3-.1 2.3-.1.2 0 .2.1.2.2 0 0 0 .1-.1.2 0 0-.5.1-1 .1H13L16.4 18l1.1-3.8c.3-1.1.6-1.9.6-2.6 0-1.1-.4-1.9-.7-2.5l-.1-.2c-.4-.6-.7-1.2-.7-1.8 0-.6.5-1.3 1.3-1.3h.6l-.5-.3c-1.6-1.5-3.8-2.3-6-2.3-1.5 0-2.9.4-4.2 1.1-1.3.6-2.4 1.6-3.2 2.8l-.2.3h.9c.9 0 2.3-.1 2.3-.1z"/></svg>
+						</li>
+						<li class="skill-item">
+							<div class="skill-item__text">
+								<h3 class="skill-item__heading item-heading">
+									Webデザイン
+								</h3>
+								<p class="skill-item__detail">
+									Figma、PS、XDなどを使用したWebデザインをしています。コーディングのしやすさやサイトとしての使いやすさを考慮したシンプルでわかりやすいデザインを得意としています。
 								</p>
 							</div>
-						</div>
-						<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
-								d="M9.3 18.4 14 5.6h.8l-4.7 12.8h-.8zM.7 12l4.9-4.9.7.7L2.1 12l4.2 4.2-.6.8-5-5zm16.9 4.2 4.2-4.2-4.2-4.2.7-.7 5 4.9-5 5-.7-.8z"/></svg>
-					</li>
-					<li class="skill-item">
-						<div class="skill-item__text">
-							<h3 class="skill-item__heading item-heading">
-								Wordpress
-							</h3>
-							<p class="skill-item__detail">
-								Wordpress歴10年以上。バージョン2.3の時代からWPでのサイト構築、オリジナルテーマを使用したサイト作成をしています。テンプレートの様々な導入パターンにも柔軟に対応可能です。
-							</p>
-						</div>
-						<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
-								d="M12 21.8c-2.6 0-5.1-1-6.9-2.9C3.2 17 2.2 14.6 2.2 12c0-2.6 1-5.1 2.9-6.9S9.4 2.2 12 2.2c2.6 0 5.1 1 6.9 2.9 1.9 1.9 2.9 4.3 2.9 6.9 0 2.6-1 5.1-2.9 6.9-1.8 1.9-4.3 2.9-6.9 2.9zm-2.7-1.3.2.1c.8.2 1.6.4 2.5.4 1 0 2-.2 3-.5l.2-.1-.2-.4-2.9-7.8-2.8 8.3zM3.9 8.4c-.5 1.1-.8 2.3-.8 3.6 0 3.4 1.9 6.5 5 8l.4.2L4.1 7.9l-.2.5zm15.9-.7c0 1-.8 3.4-1 4L16 19.9l.5-.3c1.3-.8 2.5-1.9 3.2-3.2.8-1.3 1.2-2.9 1.2-4.4 0-1.5-.4-3-1.1-4.3zM7.6 7.3c.1 0 .2.1.2.2 0 0 0 .1-.1.2 0 0-.5.1-1 .1h-.3L9.8 18l2.1-6.3-1.4-3.9h-.1c-.3 0-.6 0-.9-.1-.1 0-.2-.1-.1-.2 0 0 0-.2.2-.2.1 0 1.5.1 2.3.1.9 0 2.3-.1 2.3-.1.2 0 .2.1.2.2 0 0 0 .1-.1.2 0 0-.5.1-1 .1H13L16.4 18l1.1-3.8c.3-1.1.6-1.9.6-2.6 0-1.1-.4-1.9-.7-2.5l-.1-.2c-.4-.6-.7-1.2-.7-1.8 0-.6.5-1.3 1.3-1.3h.6l-.5-.3c-1.6-1.5-3.8-2.3-6-2.3-1.5 0-2.9.4-4.2 1.1-1.3.6-2.4 1.6-3.2 2.8l-.2.3h.9c.9 0 2.3-.1 2.3-.1z"/></svg>
-					</li>
-					<li class="skill-item">
-						<div class="skill-item__text">
-							<h3 class="skill-item__heading item-heading">
-								Webデザイン
-							</h3>
-							<p class="skill-item__detail">
-								Figma、PS、XDなどを使用したWebデザインをしています。コーディングのしやすさやサイトとしての使いやすさを考慮したシンプルでわかりやすいデザインを得意としています。
-							</p>
-						</div>
-						<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-								 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
-								d="M11.5 20.5c-3.1 0-7-1.2-9.3-2.4l-.3-.1.2-.2c2.5-2.1 2.6-3.7 2.7-5 0-.4.1-.7.2-1.1.8-3.1 3.4-5.1 6.5-5.1.6 0 1.1.1 1.7.2l.2.1 5.1-4c.1-.1.2-.1.3-.1.1 0 .3.1.4.1l3 3c.2.2.2.5 0 .7l-4 5.1.1.2c.3 1.1.3 2.3 0 3.5-.9 3-3.6 5.1-6.8 5.1zm-7.2-2.6c2.5 1 5.1 1.5 7.2 1.5 2.7 0 5.1-1.8 5.8-4.4.5-2 0-4.2-1.5-5.7l-.1-.1c-.8-.7-1.7-1.2-2.6-1.5-.5-.1-1-.2-1.6-.2-2.6 0-4.8 1.8-5.5 4.4-.1.2-.1.4-.1.9-.2 1.9-.6 3.2-1.7 4.4l-.5.5.6.2zM14.5 7.1c1.4.8 2.5 1.9 3.3 3.3l3.3-4.3-2.4-2.4-4.2 3.4z"/></svg>
-					</li>
-				</ul>
-				<a href="https://www.foriio.com/miyazakiYuuki" target="_blank" class="button">ポートフォリオ</a>
-
+							<svg class="skill-item__img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+									 style="enable-background:new 0 0 24 24" xml:space="preserve"><path
+									d="M11.5 20.5c-3.1 0-7-1.2-9.3-2.4l-.3-.1.2-.2c2.5-2.1 2.6-3.7 2.7-5 0-.4.1-.7.2-1.1.8-3.1 3.4-5.1 6.5-5.1.6 0 1.1.1 1.7.2l.2.1 5.1-4c.1-.1.2-.1.3-.1.1 0 .3.1.4.1l3 3c.2.2.2.5 0 .7l-4 5.1.1.2c.3 1.1.3 2.3 0 3.5-.9 3-3.6 5.1-6.8 5.1zm-7.2-2.6c2.5 1 5.1 1.5 7.2 1.5 2.7 0 5.1-1.8 5.8-4.4.5-2 0-4.2-1.5-5.7l-.1-.1c-.8-.7-1.7-1.2-2.6-1.5-.5-.1-1-.2-1.6-.2-2.6 0-4.8 1.8-5.5 4.4-.1.2-.1.4-.1.9-.2 1.9-.6 3.2-1.7 4.4l-.5.5.6.2zM14.5 7.1c1.4.8 2.5 1.9 3.3 3.3l3.3-4.3-2.4-2.4-4.2 3.4z"/></svg>
+						</li>
+					</ul>
+					<a href="https://www.foriio.com/miyazakiYuuki" target="_blank" class="button">ポートフォリオ</a>
+				</div>
 			</div>
 			<div class="image-area skills__images">
 				<div class="letters">
@@ -251,8 +254,7 @@
 						voici ce que<br>je peux faire
 					</div>
 				</div>
-				<img src="<?php echo get_theme_file_uri('/images/competences-portrait@2x.png'); ?>" alt="テーブルの写真"
-						 class="photo photo--large" loading="lazy">
+				<img src="<?php echo get_theme_file_uri('/images/competences-portrait@2x.png'); ?>" alt="テーブルの写真" class="photo photo--large" loading="lazy">
 			</div>
 		</section>
 		<section class="section services-prices">
@@ -260,49 +262,52 @@
 				<h2 class="section__heading">
 					サービスと料金
 				</h2>
-				<p class="services-prices__lead">
-					レッスン内容はご希望に応じます。
-					デザイン添削、コーディング添削、質問対応、Wordpress導入のお手伝いなど。<br>
-					※お申し込み方法は近日公開予定です。
-				</p>
-				<div class="services-prices__item">
-					<h3 class="services-prices__item-heading item-heading">
-						オンラインプライベートレッスン
-					</h3>
-					<p>
-						Zoomを使用したオンラインレッスンです。全国どこからでも受講可能です。<br>
+				<div class="section__wrap">
+					<p class="services-prices__lead">
+						レッスン内容はご希望に応じます。
+						デザイン添削、コーディング添削、質問対応、Wordpress導入のお手伝いなど。<br>
+						※お申し込み方法は近日公開予定です。
 					</p>
-					<p>
-						fammのWebデザイン講座で宮崎の講座を受講されたかたは <a href="<?php site_url(); ?>/private-lesson/">こちらをご覧ください</a>。<br>
-						<small>※パスワードは資料用のパスワードと同じです。</small>
-					</p>
-					<div class="prices">
-						<h4 class="prices__heading">
-							料金
-						</h4>
-						<p class="prices__item">
-							1時間 <span>5,000</span>円（税込）
+					<div class="services-prices__item">
+						<h3 class="services-prices__item-heading item-heading">
+							プライベートレッスン
+						</h3>
+						<p>
+							Zoomを使用したオンラインレッスン、もしくは対面でのレッスンです。<br>
 						</p>
-
+						<div class="prices">
+							<h4 class="prices__heading">
+								料金
+							</h4>
+							<p class="prices__item">
+								1時間 <span>5,000</span>円（税込）
+							</p>
+						</div>
+						<div class="notes">
+							<p class="notes__item">
+								※ 対面の場合には、現地までの交通費別途
+							</p>
+							<p class="notes__item">
+								※ オンラインスクールfammのWebデザイン講座で宮崎の講座を受講されたかたは <a href="<?php site_url(); ?>/private-lesson/">こちらをご覧ください</a>。
+								<small>パスワードは資料用のパスワードと同じです。</small>
+							</p>
+						</div>
 					</div>
-				</div>
-				<div class="services-prices__item">
-					<h3 class="services-prices__item-heading item-heading">
-						対面プライベートレッスン
-					</h3>
-					<p>
-						カフェなどでの対面でのオンラインレッスンです。東京都近郊の方が対象です。
-					</p>
-					<div class="prices">
-						<h4 class="prices__heading">
-							料金
-						</h4>
-						<p class="prices__item">
-							1時間 <span>5,000</span>円（税込）
+					<div class="services-prices__item">
+						<h3 class="services-prices__item-heading item-heading">
+							メンターサービス
+						</h3>
+						<p>
+							毎月定額をお支払いいただくことで、ZOOMでのオンラインレッスンとチャットでのサポートを受けられます。
 						</p>
-						<p class="prices__notes">
-							＋ 現地までの交通費
-						</p>
+						<div class="prices">
+							<h4 class="prices__heading">
+								料金
+							</h4>
+							<p class="prices__item">
+								<span>5,000</span>円（税込）/ 月～
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
