6d98d30d57b56996d431bb13a3a3924da9138442
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2023-06-27 23:24:43 +0900

    メンタリングページ

--- mentoring.php
+++ mentoring.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Template Name: プライベートレッスン
+ * Template Name: メンタリングサービス
  *
  * */
 ?>
@@ -14,9 +14,9 @@
 		</section>
 		<section class="lesson-section">
 			<div class="lesson-section__inner">
-				<p class="lesson-catch">必要な時に必要な分だけ、<br>プライベートで！</p>
+				<p class="lesson-catch">いつでも教えてくれる人がいる、<br>絶対的な安心感</p>
 				<p class="lesson-lead">
-						プライベートレッスンは、ご自身の目的や必要に応じて行う単発のレッスンです。受注中の案件でどうしてもひとりでは解決できない問題が起きたときなどにぜひご活用ください。
+					メンタリングサービスは、月々定額をお支払いいただくことで、いつでも質問できるチャットと定期ZOOMレッスンが受けられるサービスです。プライベートレッスンよりもお得に、継続して勉強できます。
 				</p>
 				<div class="lesson-button">
 					<a class="button" href="#inquiry">
@@ -32,12 +32,12 @@
 				<div class="lesson-content">
 					<div class="lesson-content__block">
 						<p>レッスンの内容は自由です。</p>
-						<p>その時その時に聞いてみたいこと、習ってみたいことを中心にお勉強していきましょう。</p>
-						<p>これからどうしていけばいいか、勉強の仕方をもう少し詳しく知りたい、などのご相談でもOKです。</p>
-						<p>お気軽にお問合せください。</p>
+						<p>案件が入ったけど、一人で納品できる自信がない！</p>
+						<p>一人だと勉強しないから定期的に見てもらいたい！</p>
+						<p>など、ご相談ください。</p>
 					</div>
 					<div class="lesson-content__block">
-						<p>継続してレッスンを受講したい場合には、よりお得な<a href="https://m-yuuki.com/mentoring/" title="Web制作メンタリングサービス">メンタリングサービス</a>をお勧めしています。
+						<p>単発でご受講を希望の方は<a href="https://m-yuuki.com/private-lesson/" title="Web制作プライベートレッスン">プライベートレッスン</a>をお勧めしています。
 						</p>
 					</div>
 				</div>
@@ -47,26 +47,53 @@
 		<section class="lesson-section lesson-section--price" id="price">
 			<div class="lesson-section__inner">
 				<h2 class="lesson-section__heading">料金</h2>
-				<div class="lesson-content">
-					<div class="lesson-box-transparent">
-						<p class="lesson-price-title">1時間</p>
-						<p class="lesson-price"><strong>5,500</strong>円</p>
+				<div class="lesson-content lesson-content--mentoring">
+					<div class="lesson-box">
+						<p class="lesson-price-title">ライトプラン</p>
+						<p class="lesson-price"><strong>5,000</strong>円</p>
+						<div class="lesson-detail">
+							<h3 class="lesson-detail__heading">プランに含まれるもの</h3>
+							<ul class="lesson-detail__list">
+								<li class="lesson-detail__list-item">ビデオ通話１時間</li>
+								<li class="lesson-detail__list-item">チャットでの質問</li>
+							</ul>
+						</div>
 					</div>
-					<div class="lesson-box-transparent">
-						<p class="lesson-price-title">初回の最初の1時間は特別価格！</p>
-						<p class="lesson-price"><strong>4,500</strong>円</p>
-						<p class="lesson-notes">※2時間目以降は通常の5,500円です</p>
+					<div class="lesson-box lesson-box--recommended">
+						<p class="lesson-box__fukidashi">
+							&emsp;おすすめ！
+						</p>
+						<p class="lesson-price-title">スタンダードプラン</p>
+						<p class="lesson-price"><strong>10,000</strong>円</p>
+						<div class="lesson-detail">
+							<h3 class="lesson-detail__heading">プランに含まれるもの</h3>
+							<ul class="lesson-detail__list">
+								<li class="lesson-detail__list-item">ビデオ通話2時間30分</li>
+								<li class="lesson-detail__list-item">チャットでの質問</li>
+								<li class="lesson-detail__list-item">ビデオ通話での作品の添削</li>
+							</ul>
+						</div>
 					</div>
-				</div>
-				<div class="lesson-button">
-					<a class="button" href="#inquiry">ご相談はこちら</a>
+					<div class="lesson-box">
+						<p class="lesson-price-title">アドバンスドプラン</p>
+						<p class="lesson-price"><strong>15,000</strong>円</p>
+						<div class="lesson-detail">
+							<h3 class="lesson-detail__heading">プランに含まれるもの</h3>
+							<ul class="lesson-detail__list">
+								<li class="lesson-detail__list-item">ビデオ通話3時間</li>
+								<li class="lesson-detail__list-item">チャットでの質問</li>
+								<li class="lesson-detail__list-item">ビデオ通話での作品の添削</li>
+								<li class="lesson-detail__list-item">チャットでの作品の添削</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
 
 		<section class="lesson-section">
 			<div class="lesson-section__inner">
-				<h2 class="lesson-section__heading">プライベートレッスンの<br class="sp-only">いいところ</h2>
+				<h2 class="lesson-section__heading">メンタリングの<br class="sp-only">いいところ</h2>
 				<ul class="feature-list">
 					<li class="feature-list__item">
 						<div class="feature-list__icon"><i class="fas fa-question-circle"></i></div>
@@ -96,19 +123,19 @@
 					<li class="feature-list__item">
 						<div class="feature-list__icon feature-list__icon--voice"><i class="fas fa-user"></i></div>
 						<p class="feature-list__text">
-							WEBデザイナーとしてデビューしたてで、とにかく何をやるにも不安！！という私の質問・疑問一つ一つに丁寧に解説していただきました。
+							コーディングのご指導はもちろん、クライアントとのやりとりで不安なことやイレギュラーへの対応など、ご経験からアドバイスをいただけて大変有難いです。
 						</p>
 					</li>
 					<li class="feature-list__item">
 						<div class="feature-list__icon feature-list__icon--voice"><i class="fas fa-user"></i></div>
 						<p class="feature-list__text">
-							ウェブサイト製作中にコーディングで行き詰まり、自分では問題に気づけなくて困っていたのですが、宮崎先生の丁寧なご指導で問題を解決することができました！
+							Webサイト制作の相談をさせてもらっております。いつも丁寧に対応してくださり、コミュニケーションを取りやすいです。
 						</p>
 					</li>
 					<li class="feature-list__item">
 						<div class="feature-list__icon feature-list__icon--voice"><i class="fas fa-user"></i></div>
 						<p class="feature-list__text">
-							ただ知識だけを教えてもらえるサイトや動画はたくさんありますが、1つ1つ例を用意して頂いたり質問形式でお話し頂けたので理解が深まったと思います。
+							納品までフォローアップありがとうございました！自分だけでは解決できなかった箇所たくさんありました。とても心強かったです！
 						</p>
 					</li>
 				</ul>
@@ -123,23 +150,25 @@
 						<dl class="lesson-flow__item">
 							<dt class="lesson-flow__item-title"><strong>STEP1</strong> まずはご相談ください</dt>
 							<dd class="lesson-flow__item-content">
-								下記のご相談フォームからご相談ください。<br>できればその際に現在のご状況やご希望のレッスン内容などをご連絡ください。<br>宮崎から改めてご連絡差し上げます。
+								下記のご相談フォームからご相談ください。
+								できればその際に現在のご状況やご希望のレッスン内容などをご連絡ください。
+								宮崎から改めてご連絡差し上げます。
 							</dd>
 						</dl>
 					</div>
 					<div class="lesson-flow__item-wrap">
 						<dl class="lesson-flow__item">
-							<dt class="lesson-flow__item-title"><strong>STEP2</strong> 日程決め・決済</dt>
+							<dt class="lesson-flow__item-title"><strong>STEP2</strong> 無料ヒアリングの実施</dt>
 							<dd class="lesson-flow__item-content">
-								メールでご都合の良い受講の日時を決めます。<br>ストアカというサイトで決済をしていただきます。日程が決定次第、決済用のリンクをお送りします。
+								30分ほどのZOOMミーティングで、ヒアリングを実施します。現在のご状況、ご希望の内容を伺い、プラン選択のアドバイスを行います。ご納得できましたら、次のステップに進んでいただきます。
 							</dd>
 						</dl>
 					</div>
 					<div class="lesson-flow__item-wrap">
 						<dl class="lesson-flow__item">
-							<dt class="lesson-flow__item-title"><strong>STEP3</strong> レッスン当日</dt>
+							<dt class="lesson-flow__item-title"><strong>STEP3</strong> ご契約・サービス開始</dt>
 							<dd class="lesson-flow__item-content">
-								決済後に届く完了メールにZOOMのリンクが記載されています。<br>当日の時間になりましたらリンクからZOOMにお入りください。
+								ストアカサイトにて、ご契約いただきます。ご契約次第、チャットにご招待します（場合によってはチャット用のメールアドレスを伺います）。初回ZOOMの日程を決め、サービス開始です。
 							</dd>
 						</dl>
 					</div>
