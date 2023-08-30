2a5df60c3f2949d1c71a2b28bc3c68e990490002
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2023-08-30 11:29:22 +0900

    フッター文言の出し分け

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <footer class="footer">
-	<small>アイコン提供：<a href="https://imagemart.aflo.com/ja/@MakotoHara" target="_blank">MakotoHara</a>/イメージマート</small><br>
+	<?php if(is_home() || is_page( array('lessons', 'mentoring', 'private-lesson', 'private-lesson-famm') )) { ?>
+		<small>アイコン提供：<a href="https://imagemart.aflo.com/ja/@MakotoHara" target="_blank">MakotoHara</a>/イメージマート</small><br>
+	<?php } else { ?>
+
+	<?php } ?>
   ©Miyazaki Y&ucirc;ki All Rights Reserved.
 </footer>
 
