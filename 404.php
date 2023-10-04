5bcacfbd67c5e863c0dd79883c9de6797aab1258
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2023-10-04 17:14:13 +0900

    404ページ追加

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+	<main class="not-found">
+		<section class="lesson-hero-section">
+			<p class="lesson-sub-heading">Not found</p>
+			<h1 class="lesson-hero-section__heading">
+				お探しのページは見つかりませんでした。
+			</h1>
+		</section>
+
+		<section class="lesson-section" id="content">
+			<div class="lesson-section__inner">
+				<div class="lesson-content">
+					<div class="lesson-content__block">
+						<p><a href="/">トップページはこちら</a></p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+<?php get_footer(); ?>
