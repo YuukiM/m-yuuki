e84bfc021dabb6561f04f2f5129b4054a81b19b0
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2024-04-26 18:50:12 +0900

    トップページの挙動がおかしかったのを修正

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+const elements = document.querySelectorAll('.js-fade-in');
+const fadeIn = function() {
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const rect = element.getBoundingClientRect();
+    const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    if (rect.top <= viewHeight) {
+      element.classList.add('visible');
+    }
+  }
+};
+document.addEventListener('scroll', fadeIn);
+
+window.addEventListener("scroll", function() {
+  var element = document.getElementById("element");
+  var shrink = window.scrollY > 0;
+  if (shrink) {
+    element.classList.add("shrink");
+  } else {
+    element.classList.remove("shrink");
+  }
+});
+
+
 function scrollFunction() {
   let $content = document.getElementById("js-content");
   let $body = document.getElementById("body");
