44468ea3364fad192d22163ee2a549fef88e8843
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2024-08-22 22:21:08 +0900

    なろう会ページ

--- page-naro-kai.php
+++ page-naro-kai.php
@@ -1,30 +1,24 @@
 <?php get_header(); ?>
-<main class="naro-kai">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <main class="naro-kai-main">
+    <img src="<?php echo get_theme_file_uri('/images/laptop.jpg'); ?>" alt="Webデザイナーになろう会">
 
-<!--    <h1>
-        <?php /*the_title(); */?>
-    </h1>-->
-
+    <section class="naro-kai-lead-container">
+      <h2 class="naro-kai-welcome">
+        「Webデザイナーになろう会」へようこそ！
+      </h2>
+      <div class="naro-kai-lead-inner">
+        <p>
+          この会では、Webデザイナーになりたい皆様のために、イベント・講座などを行っています。<br>現在は特定の方のみご加入いただけます。
+        </p>
+      </div>
+    </section>
+    <section class="naro-kai-container">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php the_content(); ?>
-
-    <?php endwhile; else: ?>
+      <?php endwhile; else: ?>
         <p>Sorry, no posts matched your criteria.</p>
-    <?php endif; ?>
-</main>
-    <style>
-        @media screen and (min-width: 768px) {
-            /* LP */
-            html {
-                font-size: 120%;
-            }
-        }
+      <?php endif; ?>
+    </section>
+  </main>
 
-        @media screen and (min-width: 1024px) {
-            /* LP */
-            html {
-                font-size: 140%;
-            }
-        }
-    </style>
 <?php get_footer(); ?>
